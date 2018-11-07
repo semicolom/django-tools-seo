4980310ee929855a36ac96886c03e09141da0a82
Toni Colom <toni@semicolom.com>
2018-11-07 23:02:12 +0100

    Fixes manifest

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tools-seo',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     description='Simple Django app to manage project SEO like Google Analytics',
