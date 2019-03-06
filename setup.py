9b16731949480fc25ad010811e528d22bf335bfb
Toni Colom <toni@semicolom.com>
2019-03-06 18:16:01 +0100

    Closes #1 adding setuppy markdown description

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tools-seo',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     description='Simple Django app to manage project SEO like Google Analytics',
     long_description=README,
+    long_description_content_type="text/markdown",
     author='Toni Colom',
     author_email='toni@semicolom.com',
     url='https://github.com/semicolom/django-tools-seo',
